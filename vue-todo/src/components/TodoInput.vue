b0dc34cf99a611caa2374e206abb15d0a6815e75
sanghoon.yoo <ggsunun567@naver.com>
2023-01-04 14:04:15 +0900

    vue and ts study by makingTodoList

---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <label for="todo-input">오늘 할 일 : </label>
+    <input
+      v-on:keyup.enter="addTodo"
+      id="todo-input"
+      type="text"
+      :value="item"
+      @input="handleInput"
+    />
+    <button @click="addTodo" tpye="button">추가</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  //props: ["item"],
+  props: {
+    item: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    handleInput(event: InputEvent) {
+      console.log(event);
+      // if (!event.target) {
+      //   return;
+      // }
+      const eventTarget = event.target as HTMLInputElement;
+      this.$emit("input", eventTarget.value);
+    },
+    addTodo() {
+      this.$emit("add");
+    },
+  },
+});
+</script>
+
+<style scoped></style>
